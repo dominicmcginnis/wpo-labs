e0d28feeb0a8b35c84a46fd81983a9b4001a43b6
Dominic McGinnis <dominicmcginnis@gmail.com>
2015-01-14 11:55:20 -0800

    Added support for all data locales as well as an input for type of metric - Average, 90th, etc...

--- wpt/scripts/buildReport.sh
+++ wpt/scripts/buildReport.sh
@@ -4,6 +4,12 @@ OUTPUT_FILE="results.html"
 
 REPORT_URL="http://srwv00dev059.stubcorp.dev:5000/superposed/display"
 
+if [ "$1" != "" ]; then
+	METRIC=$1;
+else
+	METRIC="90th%20Percentile";
+fi
+
 #SubRoutine to get the files and populate the reports 
 function getData {
 
@@ -12,54 +18,49 @@ echo "Getting data for: ${START_DATE} - ${END_DATE}"
 EVENT_TEST_PARAMS="step_1_label=unifiedEvent_cable_prod&"\
 "step_1_label=unifiedEvent_cable_prod_TX&"\
 "step_1_label=unifiedEvent_cable_prod_VA&"\
-"step_1_label_hidden=unifiedEvent_cable_prod%2CunifiedEvent_cable_prod_TX%2CunifiedEvent_cable_prod_VA&"\
+"step_1_label=unifiedEvent_cable_prod_OR&"\
+"step_1_label_hidden=unifiedEvent_cable_prod%2CunifiedEvent_cable_prod_TX%2CunifiedEvent_cable_prod_VA%2CunifiedEvent_cable_prod_OR&"\
 "step_1_start_ts=$START_DATE&step_1_end_ts=$END_DATE"
 
 HP_TEST_PARAMS="step_2_label=unifiedHP_cable_prod&"\
 "step_2_label=unifiedHP_cable_prod_TX&"\
 "step_2_label=unifiedHP_cable_prod_VA&"\
-"step_2_label_hidden=unifiedHP_cable_prod%2CunifiedHP_cable_prod_TX%2CunifiedHP_cable_prod_VA&"\
+"step_2_label=unifiedHP_cable_prod_OR&"\
+"step_2_label_hidden=unifiedHP_cable_prod%2CunifiedHP_cable_prod_TX%2CunifiedHP_cable_prod_VA%2CunifiedHP_cable_prod_OR&"\
 "step_2_start_ts=$START_DATE&step_2_end_ts=$END_DATE"
 
 SEARCH_TEST_PARAMS="step_3_label=unifiedSearchResults_cable_prod&"\
 "step_3_label=unifiedSearchResults_cable_prod_TX&"\
 "step_3_label=unifiedSearchResults_cable_prod_VA&"\
-"step_3_label_hidden=unifiedSearchResults_cable_prod%2CunifiedSearchResults_cable_prod_TX%2CunifiedSearchResults_cable_prod_VA&"\
+"step_3_label=unifiedSearchResults_cable_prod_OR&"\
+"step_3_label_hidden=unifiedSearchResults_cable_prod%2CunifiedSearchResults_cable_prod_TX%2CunifiedSearchResults_cable_prod_VA%2CunifiedSearchResults_cable_prod_OR&"\
 "step_3_start_ts=$START_DATE&step_3_end_ts=$END_DATE"
 
 TEAM_TEST_PARAMS="step_4_label=unifiedTeam_cable_prod&"\
-"step_4_label_hidden=unifiedTeam_cable_prod&"\
+"step_4_label=unifiedTeam_cable_prod_TX&"\
+"step_4_label=unifiedTeam_cable_prod_VA&"\
+"step_4_label=unifiedTeam_cable_prod_OR&"\
+"step_4_label_hidden=unifiedTeam_cable_prod%2CunifiedTeam_cable_prod_TX%2CunifiedTeam_cable_prod_VA%2CunifiedTeam_cable_prod_OR&"\
 "step_4_start_ts=$START_DATE&step_4_end_ts=$END_DATE"
-#"step_4_label=unifiedTeam_cable_prod_TX&"\
-#"step_4_label=unifiedTeam_cable_prod_VA&"\
-#"step_4_label_hidden=unifiedTeam_cable_prod%2unifiedTeam_cable_prod_TX%2unifiedTeam_cable_prod_VA&"\
-#"step_4_start_ts=$START_DATE&step_4_end_ts=$END_DATE"
-#"step_4_label_hidden=unifiedTeam_cable_prod&"\
-#"step_4_start_ts=$START_DATE&step_4_end_ts=$END_DATE"
 
 ARTIST_TEST_PARAMS="step_5_label=unifiedArtist_cable_prod&"\
-"step_5_label_hidden=unifiedArtist_cable_prod&"\
+"step_5_label=unifiedArtist_cable_prod_TX&"\
+"step_5_label=unifiedArtist_cable_prod_VA&"\
+"step_5_label=unifiedArtist_cable_prod_OR&"\
+"step_5_label_hidden=unifiedArtist_cable_prod%2CunifiedArtist_cable_prod_TX%2CunifiedArtist_cable_prod_VA%2CunifiedArtist_cable_prod_OR&"\
 "step_5_start_ts=$START_DATE&step_5_end_ts=$END_DATE"
-#"step_5_label=unifiedArtist_cable_prod_TX&"\
-#"step_5_label=unifiedArtist_cable_prod_VA&"\
-#"step_5_label_hidden=unifiedArtist_cable_prod%2unifiedArtist_cable_prod_TX%2unifiedArtist_cable_prod_VA&"\
-#"step_5_start_ts=$START_DATE&step_5_end_ts=$END_DATE"
-#"step_5_label_hidden=unifiedArtist_cable_prod&"\
-#"step_5_start_ts=$START_DATE&step_5_end_ts=$END_DATE"
 
 VENUE_TEST_PARAMS="step_6_label=unifiedVenue_cable_prod&"\
-"step_6_label_hidden=unifiedVenue_cable_prod&"\
+"step_6_label=unifiedVenue_cable_prod_TX&"\
+"step_6_label=unifiedVenue_cable_prod_VA&"\
+"step_6_label=unifiedVenue_cable_prod_OR&"\
+"step_6_label_hidden=unifiedVenue_cable_prod%2CunifiedVenue_cable_prod_TX%2CunifiedVenue_cable_prod_VA%2CunifiedVenue_cable_prod_OR&"\
 "step_6_start_ts=$START_DATE&step_6_end_ts=$END_DATE"
-#"step_6_label=unifiedVenue_cable_prod_TX&"\
-#"step_6_label=unifiedVenue_cable_prod_VA&"\
-#"step_6_label_hidden=unifiedVenue_cable_prod%2unifiedVenue_cable_prod_TX%2unifiedVenue_cable_prod_VA&"\
-#"step_6_start_ts=$START_DATE&step_6_end_ts=$END_DATE"
-#"step_6_label_hidden=unifiedVenue_cable_prod&"\
-#"step_6_start_ts=$START_DATE&step_6_end_ts=$END_DATE"
 
 XO_LANDING_TEST_PARAMS="step_7_label=xo_newxo_cable_prod_TX&"\
 "step_7_label=xo_newxo_cable_prod_VA&"\
-"step_7_label_hidden=xo_newxo_cable_prod_TX%2Cxo_newxo_cable_prod_VA&"\
+"step_7_label=xo_newxo_cable_prod_OR&"\
+"step_7_label_hidden=xo_newxo_cable_prod_TX%2Cxo_newxo_cable_prod_VA%2Cxo_newxo_cable_prod_OR&"\
 "step_7_start_ts=$START_DATE&step_7_end_ts=$END_DATE"
 
 URL_PARAMS="$EVENT_TEST_PARAMS&"\
@@ -69,7 +70,8 @@ URL_PARAMS="$EVENT_TEST_PARAMS&"\
 "$ARTIST_TEST_PARAMS&"\
 "$VENUE_TEST_PARAMS&"\
 "$XO_LANDING_TEST_PARAMS&"\
-"chart=column&table=true&metric=90th%20Percentile"
+"chart=column&table=true&metric=$METRIC"
+
 
 echo "Running: $REPORT_URL?$URL_PARAMS"
 
@@ -175,7 +177,7 @@ TIME_LABEL=""
 DAILY_REPORT="dailyReport_`date +%Y-%m-%d`.html"
 
 # Build the begging of the report file
-REPORT_SUBJECT="<div>The following is the performance aggregate report at the 90th% for `date +%Y-%m-%d+00:00:00`.</div>"
+REPORT_SUBJECT="<div>The following is the performance aggregate report at the $METRIC for `date +%Y-%m-%d+00:00:00`.</div>"
 echo "<html><head><style>table{border-collapse:collapse;border:1px solid #FF0000;}table th { border:1px solid #FF0000; }table td{border:1px solid #FF0000;}</style></head><body>${REPORT_SUBJECT}<br />" > $DAILY_REPORT
 
 #Run for the required dates
