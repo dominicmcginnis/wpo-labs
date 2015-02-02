a5056811667846b5d2be54149dd795881ab148d2
Dominic McGinnis <dominicmcginnis@gmail.com>
2015-02-02 13:43:11 -0800

    Latest changes, including adding the custom object to the window as well as putting all timings on the Performance UserTiming object

--- pageLoadStats/customTimings.js
+++ pageLoadStats/customTimings.js
@@ -1,87 +1,171 @@
-StubHubCustomLoadTiming = window.StubHubCustomLoadTiming || {};
+define('common-sh-custom-load-timing', [
+], function() {
+    'use strict';
+    SHCustomLoadTiming = window.SHCustomLoadTiming || {};
 
-//Data for ads timings
-StubHubCustomLoadTiming.adTimings = {
-    //Should be set when ads are started (instantiated)
-    "adLoadSartTime" : '',
-    "contentDisplayTimings" : {}
-}
+    //Data for ads timings
+    SHCustomLoadTiming.adTimings = {
+        //Should be set when ads are started (instantiated)
+        'adLoadSartTime' : '',
+        'contentDisplayTimings' : {}
+    };
 
-StubHubCustomLoadTiming.createAdTimingsObject = function(slotRenderTime, contentStartTime, contentLoadTime) {
-    var timingObj = {
-        "slotRenderTime" : slotRenderTime,
-        "contentLoadStartTime" : contentStartTime,
-        "contentLoadTime" : contentLoadTime
-    }
-    return timingObj;
-};
+    SHCustomLoadTiming.uxLoadTime;
 
-StubHubCustomLoadTiming.uxLoadTime;
+    SHCustomLoadTiming.measures = [];
 
-StubHubCustomLoadTiming.getPerformanceResourceTiming = function(resName) {
-    var array = performance.getEntries();
+    SHCustomLoadTiming.setAdsLoadStartTime = function(time) {
+        SHCustomLoadTiming.adTimings.adLoadSartTime = time;
+        SHCustomLoadTiming.setPerformanceUserTimeMark('ad-load-start-time');
+    };
 
-    for(var i =0; i < array.length; i++) {
-        var timingObj = array[i];
-        if(timingObj.name.indexOf(resName) > 0) {
-            console.log("Found resource: " + timingObj.name);
-            return timingObj;
+    SHCustomLoadTiming.createAdTimingsObject = function(slotRenderTime, contentStartTime, contentLoadTime) {
+        var timingObj = {
+            'slotRenderTime' : slotRenderTime,
+            'contentLoadStartTime' : contentStartTime,
+            'contentLoadTime' : contentLoadTime
+        };
+        return timingObj;
+    };
+
+    SHCustomLoadTiming.getPerformanceResourceTiming = function(resName) {
+        var array = performance.getEntries();
+
+        for (var i = 0; i < array.length; i++) {
+            var timingObj = array[i];
+            if (timingObj.name.indexOf(resName) > 0) {
+                console.debug('Found resource: ' + timingObj.name);
+                return timingObj;
+            }
         }
-    }
-    return undefined;
-}; 
+        return undefined;
+    };
 
-StubHubCustomLoadTiming.setUxLoadTimeFromResource = function(resource) {
-    var uxTargetPerformanceResourceTiming = StubHubCustomLoadTiming.getPerformanceResourceTiming(resource);
-    StubHubCustomLoadTiming.uxLoadTime = (uxTargetPerformanceResourceTiming) ? uxTargetPerformanceResourceTiming.responseEnd : undefined;
-    console.log("Perceived user performance time: " + StubHubCustomLoadTiming.uxLoadTime);
-};
-
-StubHubCustomLoadTiming.setUxLoadTimeFromElement = function(element) {
-    $(element).livequery(function () {
+    SHCustomLoadTiming.setUxLoadTimeToNow = function() {
         var now = new Date().getTime();
-        StubHubCustomLoadTiming.uxLoadTime = now - performance.timing.navigationStart;
-        console.log("New uxTimer: " + StubHubCustomLoadTiming.uxLoadTime);
-    });
-};
+        SHCustomLoadTiming.uxLoadTime = now - performance.timing.navigationStart;
 
-StubHubCustomLoadTiming.setContentDisplayTimings = function(adSlotId, timing, timeValue) {
-    var myTimings = StubHubCustomLoadTiming.adTimings.contentDisplayTimings[adSlotId.getId()]; 
-    if(myTimings) {
-        if(timing == 'contentLoadTime') {
-            timeValue = new Date().getTime() - myTimings.contentLoadStartTime;
+        SHCustomLoadTiming.setPerformanceUserTimeMark('user-ready');
+        SHCustomLoadTiming.setPerformanceUserTimeMeasure('user-ready', 'navigationStart');
+
+        console.debug('SHCustomLoadTiming: New uxTimer: ' + SHCustomLoadTiming.uxLoadTime);
+    };
+
+    SHCustomLoadTiming.setUxLoadTimeFromResource = function(resource) {
+        var uxTargetPerformanceResourceTiming = SHCustomLoadTiming.getPerformanceResourceTiming(resource);
+        SHCustomLoadTiming.uxLoadTime = (uxTargetPerformanceResourceTiming) ? uxTargetPerformanceResourceTiming.responseEnd : undefined;
+
+        if (uxTargetPerformanceResourceTiming) {
+            SHCustomLoadTiming.setPerformanceUserTimeMark('user-ready');
+            SHCustomLoadTiming.setPerformanceUserTimeMeasure('user-ready', uxTargetPerformanceResourceTiming.responseEnd);
         }
-        if(timeValue)
-            myTimings[timing] = timeValue;
-    } else {
-        myTimings = StubHubCustomLoadTiming.createAdTimingsObject('', '', '');
-        if(timeValue)
-            myTimings[timing] = timeValue;                
-    }
-    StubHubCustomLoadTiming.adTimings.contentDisplayTimings[adSlotId.getId()] =  myTimings;
-};
 
-StubHubCustomLoadTiming.setAdUnitContentDisplayLoadTime = function(adSlotId) {
-    //Set a callback for when the AdSlot's Iframe is starting to render
-    //Note: DFP adds two iframes, one for calling scripts and one for displaying the ad. we want the one that is not hidden            
-    $("#"+adSlotId.getDomId()).find("iframe:not([id*=hidden])").livequery(function(){  
-        //Establish and log our ad iframe display start time
+        console.debug('SHCustomLoadTiming: Perceived user performance time: ' + SHCustomLoadTiming.uxLoadTime);
+    };
 
-        StubHubCustomLoadTiming.setContentDisplayTimings(adSlotId, 'contentLoadStartTime', new Date().getTime());              
-        //setup our onload handler    
-        $("#"+adSlotId.getDomId()).find("iframe:not([id*=hidden])").load(function(){
-            //Tell the helper to calculate our ad display time for contentLoadTime
-            StubHubCustomLoadTiming.setContentDisplayTimings(adSlotId, 'contentLoadTime');              
+    SHCustomLoadTiming.setUxLoadTimeFromElement = function(element) {
+        $(element).livequery(function() {
+            var now = new Date().getTime();
+            SHCustomLoadTiming.uxLoadTime = now - performance.timing.navigationStart;
 
-            console.log("Ad Content display time for: " + adSlotId.getId() + " - " + StubHubCustomLoadTiming.adTimings.contentDisplayTimings[adSlotId.getId()].contentLoadTime);
+            SHCustomLoadTiming.setPerformanceUserTimeMark('user-ready');
+            SHCustomLoadTiming.setPerformanceUserTimeMeasure('user-ready', 'navigationStart');
+
+            console.debug('SHCustomLoadTiming: New uxTimer: ' + SHCustomLoadTiming.uxLoadTime);
         });
-    });   
-};
+    };
 
-StubHubCustomLoadTiming.setAdUnitSlotRenderTime = function(adSlotId) {
-    var now = new Date().getTime();
-    var slotRenderedTime = now - StubHubCustomLoadTiming.adTimings.adLoadSartTime;
+    SHCustomLoadTiming.setContentDisplayTimings = function(adSlotId, timing, timeValue) {
+        var myTimings = SHCustomLoadTiming.adTimings.contentDisplayTimings[adSlotId.getId()];
+        if (myTimings) {
+            if (timing == 'contentLoadTime') {
+                timeValue = new Date().getTime() - myTimings.contentLoadStartTime;
+            }
+            if (timeValue)
+                myTimings[timing] = timeValue;
+        } else {
+            myTimings = SHCustomLoadTiming.createAdTimingsObject('', '', '');
+            if (timeValue)
+                myTimings[timing] = timeValue;
+        }
+        SHCustomLoadTiming.adTimings.contentDisplayTimings[adSlotId.getId()] = myTimings;
+    };
 
-    StubHubCustomLoadTiming.setContentDisplayTimings(adSlotId, 'slotRenderTime', slotRenderedTime); 
-    console.log("Ad Slot Render Time for " + adSlotId.getId() + ": " + slotRenderedTime);
-};
+    SHCustomLoadTiming.setAdUnitContentDisplayLoadTime = function(adSlotId) {
+        //Set a callback for when the AdSlot's Iframe is starting to render
+        //Note: DFP adds two iframes, one for calling scripts and one for displaying the ad. we want the one that is not hidden
+        $('#'+ adSlotId.getDomId()).find('iframe:not([id*=hidden])').livequery(function() {
+            //Establish and log our ad iframe display start time
+
+            SHCustomLoadTiming.setContentDisplayTimings(adSlotId, 'contentLoadStartTime', new Date().getTime());
+
+            SHCustomLoadTiming.setPerformanceUserTimeMark(adSlotId + '-content-load-start');
+            //setup our onload handler
+            $('#'+ adSlotId.getDomId()).find('iframe:not([id*=hidden])').load(function() {
+                //Tell the helper to calculate our ad display time for contentLoadTime
+                SHCustomLoadTiming.setContentDisplayTimings(adSlotId, 'contentLoadTime');
+
+                SHCustomLoadTiming.setPerformanceUserTimeMark(adSlotId + '-content-load-time');
+                SHCustomLoadTiming.setPerformanceUserTimeMeasure(adSlotId + '-content-load-time', 'mark-' + adSlotId + '-content-load-start');
+
+                console.debug('SHCustomLoadTiming: Ad Content display time for: ' + adSlotId.getId() + ' - ' + SHCustomLoadTiming.adTimings.contentDisplayTimings[adSlotId.getId()].contentLoadTime);
+            });
+        });
+    };
+
+    SHCustomLoadTiming.setAdUnitSlotRenderTime = function(adSlotId) {
+        var now = new Date().getTime();
+        var slotRenderedTime = now - SHCustomLoadTiming.adTimings.adLoadSartTime;
+
+        SHCustomLoadTiming.setContentDisplayTimings(adSlotId, 'slotRenderTime', slotRenderedTime);
+
+        SHCustomLoadTiming.setPerformanceUserTimeMark(adSlotId + '-slot-render-time');
+        SHCustomLoadTiming.setPerformanceUserTimeMeasure(adSlotId + '-slot-render-time', 'mark-ad-load-start-time');
+
+        console.debug('SHCustomLoadTiming: Ad Slot Render Time for ' + adSlotId.getId() + ': ' + slotRenderedTime);
+    };
+
+    SHCustomLoadTiming.setPerformanceUserTimeMark = function(name) {
+        //Clear previous marks inbound mark
+        var markName = 'mark-' + name;
+        window.performance.clearMarks(markName);
+
+        //create the new marks
+        window.performance.mark(markName);
+    };
+
+    SHCustomLoadTiming.setPerformanceUserTimeMeasure = function(name, startMark) {
+        //Clear previous measures for inbound mark
+        var markName = 'mark-' + name;
+        var measureName = 'measure-' + name;
+        window.performance.clearMeasures(measureName);
+
+        //create the new measures
+        window.performance.measure(measureName, startMark, markName);
+
+        //Track custom SH measurements
+        if (SHCustomLoadTiming.measures.indexOf(name) < 0) {
+            SHCustomLoadTiming.measures.push(name);
+        }
+    };
+
+    SHCustomLoadTiming.getPerformanceMeasureTime = function(name) {
+        //Clear previous measures for inbound mark
+        var measureName = 'measure-' + name;
+        var measure = window.performance.getEntriesByName(measureName);
+
+        console.debug('Perf Time for: ' + name + ': ' + measure[0].duration);
+
+        return measure[0].duration;
+    };
+
+    SHCustomLoadTiming.getAllPerformanceMeasureTimes = function() {
+        var measureObjects = [];
+        for (var i = 0; i < SHCustomLoadTiming.measures.length; i++) {
+            SHCustomLoadTiming.getPerformanceMeasureTime(SHCustomLoadTiming.measures[i]);
+        }
+    };
+
+    return SHCustomLoadTiming;
+
+});
