19bb32b45359aafc559181d24b0c876d6e05eed1
Dominic McGinnis <dominicmcginnis@gmail.com>
2015-09-02 11:13:25 -0700

    Added a Usage function and altered some directories

--- wpt/scripts/runWPTTest.sh
+++ wpt/scripts/runWPTTest.sh
@@ -1,56 +1,79 @@
 #!/bin/sh
 
+WPT_URL="http://www.webpagetest.org"
 WPT_TEST_URL="http://www.webpagetest.org/runtest.php"
 WPT_HAR_DOWNLOAD_URL="http://www.webpagetest.org/export.php"
 WPT_TEST_STATUS_URL="http://www.webpagetest.org/testStatus.php?f=xml"
 
-#LOCATION="Dulles:Chrome.DSL"
-LOCATION="Dulles_IE9.DSL"
-URL=""
-RUNS="1"
-API_KEY=""
-PRIVATE="1"
-VIDEO="1"
-
-NOW=`date '+%Y-%m-%d-%H-%M-%S'`
-
-LABEL="default"
-
-SLEEP_DURATION=120
-
-if [ "$1" != "" ]; then
-        URL=$1;
-else
-        echo "Usage: runWPTTest.sh <URL> <label> <NumOfRuns>";
-        echo "   <URL> is a required paramater";
-        echo "   <label> is an option paramater";
-        echo "   <NumOfRuns> is an option paramater";
-        exit;
-fi
-
-if [ "$2" != "" ]; then
-        LABEL=$2;
-fi
-
-if [ "$3" != "" ]; then
-        RUNS=$3;
-fi
-
-if [ $API_KEY == "" ]; then
-	echo "You must supply an API Key";
-	exit;
-fi
+HARSTORAGE_URL="http://localhost:5000"
 
 WORKING_DIR="/Users/rmcginnis/wpt/scripts/working"
 HAR_DIR="/Users/rmcginnis/harviewer/harfiles"
 
+LOCATION="Dulles:Chrome.Cable"
+RUNS="1"
+PRIVATE="0"
+VIDEO="1"
+FVONLY="1"
+MOBILE="1"
+
+NOW=`date '+%Y-%m-%d-%H-%M-%S'`
+
+SLEEP_DURATION=120
+
+function usage {
+    echo "Usage: runWPTTest.sh <API_KEY> <TEST> <label> <NumOfRuns> <IS_SCRIPT>";
+    echo "   <API_KEY> is a required paramater";
+    echo "   <TEST> is a required paramater";
+    echo "   <label> is an optional paramater";
+    echo "   <NumOfRuns> is an optional paramater";
+    echo "   <IS_SCRIPT> is an optional paramater - set to Yes if the supplied <TEST> is a script and not a URL";
+    exit;
+}
+
+if [ "$1" != "" ]; then
+        API_KEY=$1;
+else
+	usage;
+fi
+
+if [ "$2" != "" ]; then
+        TEST=$2;
+else
+	usage;
+fi
+
+if [ "$3" != "" ]; then
+        LABEL=$3;
+fi
+
+if [ "$4" != "" ]; then
+        RUNS=$4;
+fi
+
+if [ "$5" != "" ]; then
+        IS_SCRIPT=$5;
+else 
+	IS_SCRIPT="N"
+fi
+
+
 #Replace bad filesystem chars within the files
-HAR_FILE=`echo $URL-$NOW.har |sed -e "s/\//-/g" | sed -e "s/?/-/g" | sed -e "s/=/-/g"`
-RESULT_FILE=`echo $URL-$NOW-results.xml |sed -e "s/\//-/g" | sed -e "s/?/-/g" | sed -e "s/=/-/g"`
-STATUS_FILE=`echo $URL-$NOW-status.xml |sed -e "s/\//-/g" | sed -e "s/?/-/g" | sed -e "s/=/-/g"`
+BASE_FILE=`echo $LABEL-$NOW |sed -e "s/http:\/\///g" |sed -e "s/\//-/g" | sed -e "s/?/-/g" | sed -e "s/=/-/g"`
+HAR_FILE=$BASE_FILE.har
+RESULT_FILE=$BASE_FILE-results.xml
+STATUS_FILE=$BASE_FILE-status.xml
 
-curl -s "$WPT_TEST_URL?location=$LOCATION&runs=$RUNS&f=xml&k=$API_KEY&video=$VIDEO&private=$PRIVATE&label=$LABEL&url=$URL" > $WORKING_DIR/$RESULT_FILE
+TEST_URL="$WPT_TEST_URL?location=$LOCATION&runs=$RUNS&f=xml&k=$API_KEY&video=$VIDEO&fvonly=$FVONLY&private=$PRIVATE&label=$LABEL&mobile=$MOBILE"
 
+if [ "$IS_SCRIPT" == "Y" ]; then
+    TEST_URL="$TEST_URL&script=$TEST"
+else
+    TEST_URL="$TEST_URL&url=$TEST"
+fi
+
+url=$URL
+curl -s "$TEST_URL" > $WORKING_DIR/$RESULT_FILE
 RESULT=`xmllint --xpath "/response/statusCode/text()" $WORKING_DIR/$RESULT_FILE`
 echo "Checking results file for status $WORKING_DIR/$RESULT_FILE"
 
@@ -70,17 +93,49 @@ if [ "200" -eq $RESULT ]; then
 		sleep $SLEEP_DURATION 
 
 		echo "Waking - Checking job status for test $TEST_ID"
-		curl -s "$WPT_TEST_STATUS_URL&test=$TEST_ID&k=$API_KEY" > $WORKING_DIR/$STATUS_FILE
+		curl -s "$WPT_TEST_STATUS_URL&k=$API_KEY&test=$TEST_ID" > $WORKING_DIR/$STATUS_FILE
 		STATUS=`xmllint --xpath "/response/statusCode/text()" $WORKING_DIR/$STATUS_FILE`
 	done
 
 	# Job is complete, dowload the results
 	echo "Job completed downloading har file to $HAR_DIR/$HAR_FILE"	
-	curl -s "$WPT_HAR_DOWNLOAD_URL?test=$TEST_ID&k=$API_KEY" > $HAR_DIR/$HAR_FILE
+	curl -s "$WPT_HAR_DOWNLOAD_URL?k=$API_KEY&test=$TEST_ID" > $HAR_DIR/$HAR_FILE
+
+    # Set the proper label for the test
+	cat $HAR_DIR/$HAR_FILE | sed -e "s/\"title\"/\"label\":\"$LABEL\",\"title\"/" > $HAR_DIR/$HAR_FILE.new
+	mv  $HAR_DIR/$HAR_FILE.new $HAR_DIR/$HAR_FILE
+
+    # Set a "source" element so we can click to the WPT Test from anywhere
+    #    cat $HAR_DIR/$HAR_FILE | sed -e "s#\"creator\":[{]#\"creator\":{\"source\":\"$WPT_URL/result/$TEST_ID/\",#" > $HAR_DIR/$HAR_FILE.new
+    #    mv  $HAR_DIR/$HAR_FILE.new $HAR_DIR/$HAR_FILE
+
+    # I've noticed that when using script elements and controlling the logdata steps 
+    # we sometimes get invalid har files - i.e. logging is missing.  So before we 
+    # attempt to process, lets check for validity
+    #echo "Checking for valid entries"
+    #ENTRIES_SIZE=cat $HAR_DIR/$HAR_FILE | jq '.entries | length'
+
+    #if [ "0" -eq $ENTRIES_SIZE ]; then
+    #   echo "Test failed" 
+    #   exit 100
+    #else
+    #   echo "Passed validity check"
+    #fi
+
+#	echo "Uploading to harstorage"
+#	HAR_RESPONSE=`curl -s -X POST --form "file=@$HAR_DIR/$HAR_FILE" --header "Automated: true" $HARSTORAGE_URL/results/upload`
+
+#   if [ "Successful" != "$HAR_RESPONSE" ]; then
+#       echo "Test failed, invalid HAR"
+#       exit 100
+#   fi
 
 	echo "Cleaning up working folder"
-	rm $WORKING_DIR/$RESULT_FILE 
-	rm $WORKING_DIR/$STATUS_FILE
+	#rm $WORKING_DIR/$RESULT_FILE 
+	#rm $WORKING_DIR/$STATUS_FILE
+
+	exit 0
 else
 	echo "Test failed"
+	exit 100
 fi
